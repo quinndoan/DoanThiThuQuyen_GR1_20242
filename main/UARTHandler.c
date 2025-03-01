f2b27b92906c96ba52727b76b47bec485d2b13e6
Doan Quyen <thuquyentntl@gmail.com>
2025-03-01 15:30:32 +0700

    add require 2nd

---
+++ main/UARTHandler.c
@@ -0,0 +1,80 @@
+#include "UARTHandler.h"
+#include "task_common.h"
+#include <driver/uart.h>
+#include <string.h>
+#include "Global.h"
+#define UART_PORT   1
+#define RX_BUF_SIZE     1024
+#define TXD_PIN         17
+#define RXD_PIN         16
+
+static const char* TAG = "uartTask";
+
+void initialize_uart() {
+    const uart_config_t uart_config = {
+         .baud_rate = 115200,
+         .data_bits = UART_DATA_8_BITS,
+         .parity = UART_PARITY_DISABLE,
+         .stop_bits = UART_STOP_BITS_1,
+         .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+         .source_clk = UART_SCLK_DEFAULT,
+     };
+     uart_driver_install(UART_PORT, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+     uart_param_config(UART_PORT, &uart_config);
+     uart_set_pin(UART_PORT, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+ }
+
+  void tx_task(void *arg)
+ {
+     // Nhận thông điệp từ tham số truyền vào (arg)
+     const char* message = (const char*) arg;
+ 
+         while (1){// Gửi thông điệp qua UART
+         uart_write_bytes(UART_PORT, message, strlen(message));
+         vTaskDelay(5000);
+         
+        }
+ }
+ 
+void process_uart_command(const char* command) {
+    if (strcmp(command, "who") == 0) {
+        const char* response = "Quyen's device";
+        uart_write_bytes(UART_PORT, response, strlen(response));
+        vTaskDelay(500);
+    } else if (strcmp(command, "uid") == 0) {
+        uart_write_bytes(UART_PORT, &g_uid, strlen(g_uid));
+        vTaskDelay(500);
+    } else if (strcmp(command, "atqa") == 0) {
+        uart_write_bytes(UART_PORT, &g_atqa, strlen(g_atqa));
+        vTaskDelay(500);
+    } else if (strcmp(command, "sak") == 0) {
+        uart_write_bytes(UART_PORT, &g_sak, strlen(g_sak));
+        vTaskDelay(500);
+    } else if (strcmp(command, "all")==0){
+        // send all information
+        uart_write_bytes(UART_PORT, &g_uid, strlen(g_uid));
+        vTaskDelay(500);
+        uart_write_bytes(UART_PORT, &g_atqa, strlen(g_atqa));
+        vTaskDelay(500);
+        uart_write_bytes(UART_PORT, &g_sak, strlen(g_sak));
+        vTaskDelay(500);
+    }
+    else {
+        const char* response = "Unknown command";
+        uart_write_bytes(UART_PORT, response, strlen(response));
+    }
+}
+
+void rx_task(void *arg) {
+    static const char *RX_TASK_TAG = "RX_TASK";
+    uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE + 1);
+    while (1) {
+        const int rxBytes = uart_read_bytes(UART_PORT, data, RX_BUF_SIZE, 500);
+        if (rxBytes > 0) {
+            data[rxBytes] = '\0';
+          //  ESP_LOGI(TAG, "Read bytes from flash");
+            process_uart_command((const char*)data);
+        }
+    }
+    free(data);
+}
