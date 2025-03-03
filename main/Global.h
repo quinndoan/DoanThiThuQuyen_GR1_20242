cfa4d76692cd56dbe025400464d121ddeb70b45e
Doan Quyen <thuquyentntl@gmail.com>
2025-03-03 22:19:48 +0700

    Revert "add connect to UART successfully"
    This reverts commit 33e7694a107dc7b161562fb15192dc35fb1794b0.

--- main/Global.h
+++ main/Global.h
@@ -1,12 +1,11 @@
-#ifndef GLOBAL_H
-#define GLOBAL_H
-
 #include "string.h"
+
+char g_uid[20];
+char g_atqa[20];
+char g_sak[20];
 
 // NVS namespace và keys
 #define NVS_NAMESPACE "rfid_data"
 #define NVS_KEY_UID "uid"
 #define NVS_KEY_ATQA "atqa"
 #define NVS_KEY_SAK "sak"
-
-#endif
